fab994b6e2283be41d3382953cde811d0f18d639
Elodie S <elodie.sevestre86@gmail.com>
2026-03-10 16:06:36 +0100

    essai json

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
+//! comme on travaille sur vite et non node.js pas besoin d'import les modules fs et parser le JSON
+
+// Import
+
 import "./style.css";
 import quiz from "./quiz-femmes-scientifiques.json";
 
-document.querySelector("#app > h1").innerText = quiz.title;
+// Affichage titre quizz sur écran d'acceuil (+ écran questions ?)
+document.querySelector(".welcome > h1").innerText = quiz.title;
