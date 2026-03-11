5c6997013af90151af3146b4bc80568e06330d40
Gaed <oger.gaedic@gmail.com>
2026-03-11 11:24:48 +0100

    Ajout id button et pages JS start and score

---
+++ src/score.js
@@ -0,0 +1,5 @@
+//Récupérer les réponses aux questions: combien de bonnes réponses?
+//Afficher le score
+//Afficher le nombre de questions réussies
+//Récupérer le bouton recommencer
+//Ecouter le bouton récommencer pour réinitialiser le questionnaire
