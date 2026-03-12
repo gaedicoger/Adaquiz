971ff5edc202ea50e9a29a809d595337d1c220fd
walidira <drouet_49@outlook.fr>
2026-03-12 15:39:11 +0100

    barre de progression

--- src/progression.js
+++ src/progression.js
@@ -1,9 +1,15 @@
-// affiche d'une barre de progression en fanction du score
+// affiche d'une barre de progression en fonction du score
 
+const totalQuestions = 10;
 let questionActuelle = 1;
-mettreAJourProgression();
 
-function questionSuivante() {
+function mettreAJourProgression() {
+  const barre = document.getElementById("displayProgress");
+  const pourcentage = (questionActuelle / totalQuestions) * 100;
+  barre.style.width = `${pourcentage}%`;
+}
+
+export function questionSuivante() {
   if (questionActuelle < totalQuestions) {
     questionActuelle++;
     mettreAJourProgression();
