d1028c296b5ed9fd5c078b0e8c50f5f65af67649
Elodie S <elodie.sevestre86@gmail.com>
2026-03-13 09:27:14 +0100

    affichage quizz : pseudo code

--- src/quiz-display.js
+++ src/quiz-display.js
@@ -1,1 +1,7 @@
 // ======================= AFFICHAGE DU QUIZZ =======================
+
+// afficher le quizz quand l'utilisateur clique sur commencer
+// pour chaque question, récupérer la question et les réponses dans le JSON
+// quand l'utilisateur clique sur le bouton suivante, itérer sur JSON
+// à la dernière question, transformer le bouton question suivante en bouton Score
+// le bouton Score affiche l'écran de fin
