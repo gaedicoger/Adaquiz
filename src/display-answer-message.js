fe548acfbc96e3ae0e2d60f93a1796302baf5ff8
Gaed <oger.gaedic@gmail.com>
2026-03-13 12:11:47 +0100

    mise au propre commentaires installation des import et export

--- src/display-answer-message.js
+++ src/display-answer-message.js
@@ -1,3 +1,22 @@
+// ============================= IMPORTS ======================================
+import score from {"./main.js"}
+
+// ============================= VARIABLES ====================================
+//récupérer les index ?
+
+//Fonction calcul du score:
+/**
+ *
+ * @param {*} indexUserAnswer
+ * @param {*} correctIndex
+ */
+export function calculScore(indexUserAnswer, correctIndex) {
+  //quand je clique sur une réponse sur un bouton je stocke l'index de la réponse trouver comment le stocker sur la page bouton ?
+  if (indexUserAnswer === correctIndex) {
+    score += 1;
+  }
+}
+
 //Fonction affichage des bonnes et des mauvaises réponses :
 /**
  *
@@ -8,6 +27,7 @@
 export function displayAnswerMessage(indexUserAnswer, correctIndex) {
   const answerMessage = document.getElementById("answerMessage");
   const messageText = document.createElement("p");
+  //ajouter une création de class pour gérer le style dans le CSS?
 
   if (indexUserAnswer === correctIndex) {
     //Comparer les index pour voir la bonne réponse
@@ -17,5 +37,6 @@ export function displayAnswerMessage(indexUserAnswer, correctIndex) {
     messageText.textContent = "❌ Mauvaise réponse...";
     messageText.style.color = "salmon";
   }
+  // @ts-ignore
   answerMessage.appendChild(messageText);
 }
--- src/score.js
+++ src/score.js
@@ -1,41 +1,27 @@
-//IMPORTS:
+// ============================= IMPORTS ======================================
 import quiz from "./quiz-femmes-scientifiques.json";
-//import ? score from "./es-questions.js"
+import score from "./main.js";
 
-//VARIABLES:
+// ============================= VARIABLES ====================================
 //Stocker le nombre de questions en tout:
 const numberQuestions = quiz.questions.length;
 //Stocker le score:
-let score = 0;
+let finalScore = score;
 //Stocker le pourcentage:
 let pourcentScore = 0;
-//Stocker l'index de la réponse utilisateur :
-let indexUserAnswer = ;
 
 // ===========================================================================
 // ==============================  CALCUL ==========================================
 // ===========================================================================
-//Fonction calcul du score:
-/**
- * 
- * @param {*} indexUserAnswer 
- * @param {*} correctIndex 
- */
-function calculScore (indexUserAnswer, correctIndex) { 
-//quand je clique sur une réponse sur un bouton je stocke l'index de la réponse trouver comment le stocker sur la page bouton ?
-if (indexUserAnswer === correctIndex) {
-    score +=1;
-  }
-}
 
 //Fonction transformer le score en pourcentage. selon le nombre de questions
 /**
- * 
- * @param {*} score 
- * @param {*} numberQuestions 
+ *
+ * @param {*} finalScore
+ * @param {*} numberQuestions
  */
-function calculPourcentScore (score, numberQuestions) {
-pourcentScore = (score / numberQuestions)*100;
+function calculPourcentScore(finalScore, numberQuestions) {
+  pourcentScore = (finalScore / numberQuestions) * 100;
 }
 
 // ===========================================================================
@@ -44,57 +30,58 @@ pourcentScore = (score / numberQuestions)*100;
 
 //Fonction pour afficher le score :
 /**
- * 
- * @param {*} score 
+ *
+ * @param {*} score
  */
-function displayScore (score) {
-let resultScore=document.getElementById("resultScore")
-resultScore.innerText=`Ton score est de ${score} points`
+function displayScore(finalScore) {
+  let resultScore = document.getElementById("resultScore");
+  resultScore.innerText = `Ton score est de ${finalScore} points`;
 }
 
 //Fonction Afficher les message en fonction du score en pourcentage:
 /**
- * 
- * @param {*} pourcentScore 
+ *
+ * @param {*} pourcentScore
  */
 
-function displayScoreMessage (pourcentScore) { 
-  let scoreMessage= document.getElementById("scoreMessage")//récupérer l'emplacement du score
-  if (pourcentScore===100) {
-    scoreMessage.innerText= `Aucune erreur, c'est parfait 😎`; //Remplacer le message par le score à jour selon le score ou le pourcentage de réussite
-    } else if (pourcentScore >=80 && pourcentScore<100) {
-      scoreMessage.innerText= `C'est bien, tu as fait peu d'erreurs 😉`
-    } else if (pourcentScore >=50 && pourcentScore<80 ) {
-      scoreMessage.innerText= `C'est pas mal, mais tu peux encore t'améliorer 💪`
-    } else if (pourcentScore<50) {
-      scoreMessage.innerText=`Aïe, tu as beaucoup d'erreurs, tu devrais réessayer 😅`
-    } else {scoreMessage.innerText= `Oups ! Tu n'as trouvé aucune bonne réponse 😱`};
+function displayScoreMessage(pourcentScore) {
+  const scoreMessagePlace = document.getElementById("scoreMessage"); //récupérer l'emplacement du score
+  let scoreMessage = document.createElement("p");
+  //ajouter une création de class pour gérer le style dans le CSS?
+  if (pourcentScore === 100) {
+    scoreMessage.innerText = `Aucune erreur, c'est parfait 😎`; //Remplacer le message par le score à jour selon le score ou le pourcentage de réussite
+  } else if (pourcentScore >= 80 && pourcentScore < 100) {
+    scoreMessage.innerText = `C'est bien, tu as fait peu d'erreurs 😉`;
+  } else if (pourcentScore >= 50 && pourcentScore < 80) {
+    scoreMessage.innerText = `C'est pas mal, mais tu peux encore t'améliorer 💪`;
+  } else if (pourcentScore > 0 && pourcentScore < 50) {
+    scoreMessage.innerText = `Aïe, tu as beaucoup d'erreurs, tu devrais réessayer 😅`;
+  } else {
+    scoreMessage.innerText = `Oups ! Tu n'as trouvé aucune bonne réponse 😱`;
+  }
+  scoreMessagePlace.appendChild(scoreMessage);
 }
-
 
 //Fonction pour afficher le nombre de questions réussies:
 /**
- * 
- * @param {*} score 
- * @param {*} numberQuestions 
+ *
+ * @param {*} finalScore
+ * @param {*} numberQuestions
  */
 
-function displayTotalScore (score, numberQuestions) {
-  const displayTotalScore = document.getElementById("totalScore")
-  displayTotalScore.innerText=` Tu as trouvé la bonne réponse pour ${score} questions sur ${numberQuestions}`;
+function displayTotalScore(finalScore, numberQuestions) {
+  const displayTotalScore = document.getElementById("totalScore");
+  displayTotalScore.innerText = ` Tu as trouvé la bonne réponse pour ${finalScore} questions sur ${numberQuestions}`;
 }
 // ===========================================================================
 // ==============================  FINISH ==========================================
 // ===========================================================================
 
-
-export function endScreen () {
+export function endScreen() {
   const endScreen = document.querySelector(".endScreen");
   endScreen.classList.remove("hidden"); //afficher l’écran de fin
-  calculScore(indexUserAnswer, correctIndex);
-  calculPourcentScore (score, numberQuestions);
-  displayScore (score);
-  displayScoreMessage (pourcentScore);
-  displayTotalScore (score, numberQuestions);
+  calculPourcentScore(finalScore, numberQuestions); //calculer le pourcentage de réussite
+  displayScore(finalScore); //afficher le score final
+  displayScoreMessage(pourcentScore); //affichaer le message réussite
+  displayTotalScore(finalScore, numberQuestions); //afficher le nomvbre de squestions réussies
 }
-
