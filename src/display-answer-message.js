57632a2869daa178df0f8460d9fa83ab253b1c27
Gaed <oger.gaedic@gmail.com>
2026-03-13 11:27:41 +0100

    ajout page avec fonction affichage reponses

---
+++ src/display-answer-message.js
@@ -0,0 +1,21 @@
+//Fonction affichage des bonnes et des mauvaises réponses :
+/**
+ *
+ * @param {*} indexUserAnswer
+ * @param {*} correctIndex
+ */
+
+export function displayAnswerMessage(indexUserAnswer, correctIndex) {
+  const answerMessage = document.getElementById("answerMessage");
+  const messageText = document.createElement("p");
+
+  if (indexUserAnswer === correctIndex) {
+    //Comparer les index pour voir la bonne réponse
+    messageText.textContent = "✅ Bonne réponse !";
+    messageText.style.color = "lightgreen";
+  } else {
+    messageText.textContent = "❌ Mauvaise réponse...";
+    messageText.style.color = "salmon";
+  }
+  answerMessage.appendChild(messageText);
+}
