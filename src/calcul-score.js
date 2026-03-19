d6fb1e772dbffaa40192bab7bb86279d14971dd4
Elodie S <elodie.sevestre86@gmail.com>
2026-03-19 18:33:15 +0100

    doc calculScore()

--- src/calcul-score.js
+++ src/calcul-score.js
@@ -8,14 +8,14 @@ import quiz from "./quiz-femmes-scientifiques.json";
 //Récupérer et stocker les deux index dans une variable:  doit être dans main.js avec la fonction calcul de score
 
 export let score = 0;
-// export let currentQuestionIndex = 0;
 
-//Fonction calcul du score:
 /**
- *
+ * - calcul le score
+ * - affiche "bonne ou mauvaise" réponse
  * @param {*} indexUserAnswer
  * @param {*} correctIndex
  */
+
 export function calculScore(indexUserAnswer, correctIndex) {
   //quand je clique sur une réponse sur un bouton je stocke l'index de la réponse trouver comment le stocker sur la page bouton ?
   if (indexUserAnswer === correctIndex) {
