642b0f453a03336d8fc53492bbc06c2f0632d909
walidira <drouet_49@outlook.fr>
2026-03-19 09:47:53 +0100

    finission et ajout

--- src/main.js
+++ src/main.js
@@ -1,23 +1,18 @@
 //! comme on travaille sur vite et non node.js pas besoin d'import les modules fs et parser le JSON
 
 // Import
-
 import "./style.css";
 import "./start.js";
 import "./btn-next-question.js";
 import "./btn-retry-quiz.js";
-import "./calcul-score.js";
 import "./display-end-screen.js";
 import "./progression.js";
-import "./quiz-display.js";
-import quiz from "./quiz-femmes-scientifiques.json";
-import "./systeme-de-vie.js";
 import "./local-storage.js";
 
-//! les import ci-dessous suppriment le style CSS de Vite....
-//import { initBtnNext } from "./btn-next-question.js";
-//import { questionSuivante } from "./progression.js"; // syntaxe à favoriser pour cibler une fonction définie dans le .js, permet de minimiser le nombre d'import
-//import { startQuiz } from "./quiz-display.js";
+// import quiz from "./quiz-femmes-scientifiques.json";
+// import { startQuiz } from "./quiz-display.js";
+// import {s resetScore } from "./calcul-score.js";
+import { initLives, resetLife } from "./systeme-de-vie.js";
 
 // ========================= AFFICHAGE PAR DEFAUT ==========================
 
@@ -28,14 +23,28 @@ const displayEndScreen = document.querySelector(".endScreen");
 
 // displayStartScreen.classList.add("hidden");
 
-// //* masquer par l'écran quiz et écran de fin
-
+// //* masquer l'écran quiz et l'écran de fin
 displayQuizScreen.classList.add("hidden");
 displayEndScreen.classList.add("hidden");
 
-// ======================= Ecran d'acceuil =========================
+// ======================= Ecran d'accueil =========================
 
-//* affichage titre quizz sur écran d'acceuil
+//* affichage titre quizz sur écran d'accueil
 document.querySelector(".welcome > h1").innerText = quiz.title;
 
 // ========================= Ecran quizz ===========================
+
+// initialisation des vies au chargement
+initLives();
+
+// fonction pour relancer le quiz
+export function restartQuiz() {
+  resetScore();
+  resetLife();
+
+  displayStartScreen.classList.add("hidden");
+  displayEndScreen.classList.add("hidden");
+  displayQuizScreen.classList.remove("hidden");
+
+  startQuiz();
+}
