8c3ae5ada1e8a83c4ca3e29a3a60710603fa6777
Elodie S <elodie.sevestre86@gmail.com>
2026-03-19 18:50:31 +0100

    doc updateProgressBar()

--- src/progression.js
+++ src/progression.js
@@ -3,6 +3,11 @@ import quiz from "./quiz-femmes-scientifiques.json";
 const totalQuestions = quiz.questions.length;
 export let currentQuestion = 0;
 
+/**
+ * - affiche barre de progression
+ * - incrémente son avancement
+ */
+
 export function updateProgressBar() {
   const barre = document.getElementById("display-progress");
 
